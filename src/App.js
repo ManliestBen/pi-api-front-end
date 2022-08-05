f23f79c06a06570900ce831559757474f936e912
Ben Manley <benjamintmanley@gmail.com>
2022-08-05 10:18:35 -0500

    update HTML/CSS

--- src/App.js
+++ src/App.js
@@ -3,31 +3,37 @@ import './App.css';
 function App() {
   return (
   <div class="container">
-    <div class="nav"></div>
+    <div class="nav">API Fun with a Rasperry Pi</div>
     <div class="video-container">
-      <div class="video"></div>
-      <div class="video-description"></div>
+      <div class="video">
+
+      </div>
+      <div class="video-description">Video Description</div>
     </div>
     <div class="color-control-container">
-      <div class="color-control-header"></div>
-      <div class="color-control-button-container"></div>
+      <div class="color-control-header">RGB LED Color Controls</div>
+      <div class="color-control-button-container">
+        <button>Send Colors</button>
+      </div>
       <div class="color-control"></div>
     </div>
     <div class="oled-text-control-container">
-      <div class="oled-text-control-header"></div>
+      <div class="oled-text-control-header">OLED Text Controls</div>
       <div class="oled-text-area"></div>
-      <div class="oled-text-button-container"></div>
+      <div class="oled-text-button-container">
+        <button>Send Message</button>
+      </div>
     </div>
     <div class="oled-extra-commands">
-      <div class="oled-extra-commands-header"></div>
-      <div class="oled-extra-command-button-1"></div>
-      <div class="oled-extra-command-button-2"></div>
-      <div class="oled-extra-command-button-3"></div>
-      <div class="oled-extra-command-button-4"></div>
-      <div class="oled-extra-command-button-5"></div>
-      <div class="oled-extra-command-button-6"></div>
-      <div class="oled-extra-command-button-7"></div>
-      <div class="oled-extra-command-button-8"></div>
+      <div class="oled-extra-commands-header">Other OLED Commands</div>
+      <div class="oled-extra-command-button-1"><button>Send Command</button></div>
+      <div class="oled-extra-command-button-2"><button>Send Command</button></div>
+      <div class="oled-extra-command-button-3"><button>Send Command</button></div>
+      <div class="oled-extra-command-button-4"><button>Send Command</button></div>
+      <div class="oled-extra-command-button-5"><button>Send Command</button></div>
+      <div class="oled-extra-command-button-6"><button>Send Command</button></div>
+      <div class="oled-extra-command-button-7"><button>Send Command</button></div>
+      <div class="oled-extra-command-button-8"><button>Send Command</button></div>
     </div>
   </div>
   );
