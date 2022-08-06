d3f3e517550e00707ef9a5d4f9910d0b5e79c85a
Ben Manley <benjamintmanley@gmail.com>
2022-08-06 16:35:42 -0500

    add rgb color picker, implement video

--- src/App.js
+++ src/App.js
@@ -1,39 +1,50 @@
 import './App.css';
+import { useState } from 'react';
+import { RgbColorPicker } from 'react-colorful';
+import * as apiService from './services/api-calls'
 
 function App() {
-  return (
-  <div class="container">
-    <div class="nav">API Fun with a Rasperry Pi</div>
-    <div class="video-container">
-      <div class="video">
+  const [color, setColor] = useState({ r: 50, g: 100, b: 150 });
 
+  const handleSendRGBData = async () => {
+    await apiService.sendRGBData(color)
+  }
+
+  return (
+  <div className="container">
+    <div className="nav">API Fun with a Rasperry Pi</div>
+    <div className="video-container">
+      <div className="video">
+        <img src="http://manliestben.zapto.org:8000/stream.mjpg" alt="" width="640" height="480" />
       </div>
-      <div class="video-description">Video Description</div>
+      <div className="video-description">Video Description</div>
     </div>
-    <div class="color-control-container">
-      <div class="color-control-header">RGB LED Color Controls</div>
-      <div class="color-control-button-container">
-        <button>Send Colors</button>
+    <div className="color-control-container">
+      <div className="color-control-header">RGB LED Color Controls</div>
+      <div className="color-control-button-container">
+        <button onClick={() => handleSendRGBData()}>Send Color</button>
       </div>
-      <div class="color-control"></div>
+      <div className="color-control">
+        <RgbColorPicker color={color} onChange={setColor} />
+      </div>
     </div>
-    <div class="oled-text-control-container">
-      <div class="oled-text-control-header">OLED Text Controls</div>
-      <div class="oled-text-area"></div>
-      <div class="oled-text-button-container">
+    <div className="oled-text-control-container">
+      <div className="oled-text-control-header">OLED Text Controls</div>
+      <div className="oled-text-area"></div>
+      <div className="oled-text-button-container">
         <button>Send Message</button>
       </div>
     </div>
-    <div class="oled-extra-commands">
-      <div class="oled-extra-commands-header">Other OLED Commands</div>
-      <div class="oled-extra-command-button-1"><button>Send Command</button></div>
-      <div class="oled-extra-command-button-2"><button>Send Command</button></div>
-      <div class="oled-extra-command-button-3"><button>Send Command</button></div>
-      <div class="oled-extra-command-button-4"><button>Send Command</button></div>
-      <div class="oled-extra-command-button-5"><button>Send Command</button></div>
-      <div class="oled-extra-command-button-6"><button>Send Command</button></div>
-      <div class="oled-extra-command-button-7"><button>Send Command</button></div>
-      <div class="oled-extra-command-button-8"><button>Send Command</button></div>
+    <div className="oled-extra-commands">
+      <div className="oled-extra-commands-header">Other OLED Commands</div>
+      <div className="oled-extra-command-button-1"><button>Send Command</button></div>
+      <div className="oled-extra-command-button-2"><button>Send Command</button></div>
+      <div className="oled-extra-command-button-3"><button>Send Command</button></div>
+      <div className="oled-extra-command-button-4"><button>Send Command</button></div>
+      <div className="oled-extra-command-button-5"><button>Send Command</button></div>
+      <div className="oled-extra-command-button-6"><button>Send Command</button></div>
+      <div className="oled-extra-command-button-7"><button>Send Command</button></div>
+      <div className="oled-extra-command-button-8"><button>Send Command</button></div>
     </div>
   </div>
   );
--- src/services/api-calls.js
+++ src/services/api-calls.js
@@ -1,7 +1,8 @@
-const BASE_URL = process.env.DYNDNS
+const BASE_URL = process.env.REACT_APP_DYNDNS
 
 async function sendRGBData(rgbData) {
-  const res = await fetch(`BASE_URL${`:5001`}`, {
+  const res = await fetch(`${BASE_URL}:5001/post_json`, {
+    method: 'POST',
     body: JSON.stringify(rgbData),
     headers: {'Content-Type': 'application/json'}
   })
